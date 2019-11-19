2dfc9bcf2b89a22c6eddf0efd8db92d51e7d992d
Steven Kim <sskim14@gmail.com>
2019-11-19 10:52:42 -0500

    Added necessary dependencies

---
+++ src/components/Checkbox/Checkbox.stories.js
@@ -0,0 +1,11 @@
+// import React from "react";
+// import { storiesOf } from "@storybook/react";
+
+// import Checkbox from "./Checkbox";
+
+// storiesOf("Checkbox", module)
+//     .add("checkbox", () => <Checkbox
+//         type="checkbox"
+//         checked={this.props.checked}
+//         onChange={this.props.handleCheckboxChange}
+//     />)
