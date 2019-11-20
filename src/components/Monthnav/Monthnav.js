1c7075b828026438b691d17702897b2e3206368a
Steven Kim <sskim14@gmail.com>
2019-11-19 22:08:57 -0500

    Initializing carousel for months display

--- src/components/Monthnav/Monthnav.js
+++ src/components/Monthnav/Monthnav.js
@@ -5,7 +5,18 @@ const Monthnav = () => {
     return (
         <div className="monthContainer">
             <button className="arrow previous">&#8249;</button>
-            <span className="month">January</span>
+                <span className="month">January</span>
+                <span className="month">February</span>
+                <span className="month">March</span>
+                <span className="month">April</span>
+                <span className="month">May</span>
+                <span className="month">June</span>
+                <span className="month">July</span>
+                <span className="month">August</span>
+                <span className="month">September</span>
+                <span className="month">October</span>
+                <span className="month">November</span>
+                <span className="month">December</span>
             <button className="arrow next">&#8250;</button>
         </div>
     )
