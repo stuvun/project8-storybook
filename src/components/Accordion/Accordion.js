9fb783b3d1b13aa8804aca420414b5158cc087eb
Steven Kim <sskim14@gmail.com>
2019-11-19 21:36:22 -0500

    Styled Accordion and added primary colors

--- src/components/Accordion/Accordion.js
+++ src/components/Accordion/Accordion.js
@@ -4,13 +4,18 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 const Accordion = (props) => {
     let classList = ""
+    let containerList = ""
     let types = [ "default", "primary" ]
+    let labels = [ "default", "primary" ]
 
     if (types.includes(props.type)) {
         classList += ` burger-${props.type}`
     }
+    if (labels.includes(props.label)) {
+        containerList += ` hamburger-${props.label}`
+    }
     return ( 
-        <div className="hamburger">
+        <div className={ containerList }>
             <div className={ classList }></div>
             <div className={ classList }></div>
             <div className={ classList }></div>
--- src/components/Accordion/Accordion.stories.js
+++ src/components/Accordion/Accordion.stories.js
@@ -5,7 +5,9 @@ import Accordion from "./Accordion";
 storiesOf("Accordion", module)
     .add("Default", () => <Accordion
         type="default"
+        label="default"
         />)
     .add('Primary', () => <Accordion
         type="primary"
+        label="primary"
     />)
